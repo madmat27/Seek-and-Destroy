f2c7dea075e3c9ea572e6e2e0e1a69d324d3b056
Mariana S. Mz <mscmazi@gmail.com>
2016-10-13 11:54:16 +0300

    v3.00
    More testing...

--- seekndestroy.js
+++ seekndestroy.js
@@ -2,30 +2,25 @@ function destroyer(arr) {
   // Remove all the values
   
   var args = Array.prototype.slice.call(arguments);
+  var temparr = [];
   var i;
   var j;
   
   for (i = 0; i < args.length; i++) {
       if (Array.isArray(args[i])) {
-        var temparr = args.splice(i,1);
+        temparr = args.splice(i,1);
         }
     }
     
   for (i=0; i < arr.length; i++) {
-    for (j=
+    for (j= 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        temparr = arr.splice(i,1);
+        }
+      }
     }
-  
-  /*
-  array.splice(i, 1);
-
-If you want to remove every element with value number from the array:
-
-for(var i = array.length - 1; i >= 0; i--) {
-    if(array[i] === number) {
-       array.splice(i, 1);
-    }
-}---
-
+    
+  return arr;
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
