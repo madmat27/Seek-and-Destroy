fb191bcca4d8f9d23211c0c18b0291be8f91f0b5
Mariana S. Mz <mscmazi@gmail.com>
2016-10-13 11:55:46 +0300

    v4.01
    Working with filter()

--- seekndestroy.js
+++ seekndestroy.js
@@ -1,31 +1,11 @@
-
-// Working but with no .filter method: 
 function destroyer(arr) {
   // Remove all the values
   
   var args = Array.prototype.slice.call(arguments);
-  var i;
-  var j;
-  
-  for (i = 0; i < args.length; i++) {
-      if (Array.isArray(args[i])) {
-        args.splice(i,1);
-        }
-    }
-    
-  for (j=0; j < args.length; j++) {
-    for (i= 0; i < arr.length; i++) {
-      if (arr[i] === args[j] && i !== 0) {
-        arr.splice(i,1);
-        i--;
-        }
-        else if (arr[i] === args[j] && i === 0) {
-          arr.splice(i,1);
-        }
-      }
-    }
-    
-  return arr;
+
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
 }
 
-destroyer([5,6], 6, 5);
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
