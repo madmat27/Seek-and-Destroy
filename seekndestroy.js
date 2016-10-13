ef063515608e08a0bc797e1e784572a5bdc39041
Mariana S. Mz <mscmazi@gmail.com>
2016-10-13 11:52:07 +0300

    v1.00
    First try....

---
+++ seekndestroy.js
@@ -0,0 +1,18 @@
+function destroyer(arr) {
+  // Remove all the values
+  
+  var args = Array.prototype.slice.call(arguments);
+  
+  for (var i = 0; i < args.length; i++) {
+      if (Array.isArray(args[i])) {
+        var temparr = args[i];
+        }
+    }
+  
+  function remove(value) {
+     return 
+    }
+
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
